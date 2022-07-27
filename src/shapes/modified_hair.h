479be466790744afd024cbd1f0685b8152fb6e13
playerzhou <playerzhou@icloud.com>
2022-07-27 10:26:47 +0800

    update

---
+++ src/shapes/modified_hair.h
@@ -0,0 +1,101 @@
+/*
+    This file is part of Mitsuba, a physically based rendering system.
+
+    Copyright (c) 2007-2014 by Wenzel Jakob and others.
+
+    Mitsuba is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License Version 3
+    as published by the Free Software Foundation.
+
+    Mitsuba is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#if !defined(__MODIFIED_HAIR_H)
+#define __MODIFIED_HAIR_H
+
+#include <mitsuba/render/shape.h>
+
+MTS_NAMESPACE_BEGIN
+
+class ModifiedHairKDTree;
+
+/**
+ * \brief Intersection shape structure for cylindrical hair
+ * segments with miter joints. This class expects an ASCII file containing
+ * a list of hairs made from segments. Each line should contain an X,
+ * Y and Z coordinate separated by a space. An empty line indicates
+ * the start of a new hair.
+ */
+class ModifiedHairShape : public Shape {
+public:
+    /// Construct a new ModifiedHairShape instance given a properties object
+    ModifiedHairShape(const Properties& props);
+
+    /// Unserialize from a binary data stream
+    ModifiedHairShape(Stream* stream, InstanceManager* manager);
+
+    /// Serialize to a binary data stream
+    void serialize(Stream* stream, InstanceManager* manager) const;
+
+    // =============================================================
+    //! @{ \name Access the internal vertex data
+    // =============================================================
+
+    /// Return the list of vertices underlying the hair shape
+    const std::vector<Point>& getVertices() const;
+
+    /**
+     * Return a boolean list specifying whether a vertex
+     * marks the beginning of a new fiber
+     */
+    const std::vector<bool>& getStartFiber() const;
+
+    //! @}
+    // =============================================================
+
+    // =============================================================
+    //! @{ \name Implementation of the \ref Shape interface
+    // =============================================================
+
+    bool rayIntersect(const Ray& ray, Float mint,
+        Float maxt, Float& t, void* temp) const;
+
+    bool rayIntersect(const Ray& ray, Float mint, Float maxt) const;
+
+    void fillIntersectionRecord(const Ray& ray,
+        const void* temp, Intersection& its) const;
+
+    ref<TriMesh> createTriMesh();
+
+    const KDTreeBase<AABB>* getKDTree() const;
+
+    AABB getAABB() const;
+
+    Float getSurfaceArea() const;
+
+    size_t getPrimitiveCount() const;
+
+    size_t getEffectivePrimitiveCount() const;
+
+    //! @}
+    // =============================================================
+
+    /// Return a human-readable representation
+    std::string toString() const;
+
+    MTS_DECLARE_CLASS()
+private:
+    ref<ModifiedHairKDTree> m_kdtree;
+    Float m_interpolation_thredhold;
+
+};
+
+MTS_NAMESPACE_END
+
+#endif /* __HAIR_H */
